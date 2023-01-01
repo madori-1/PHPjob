7dc115c2a7b08129da3014c0d387deb83f615953
madori-1 <64700675+madori-1@users.noreply.github.com>
2023-01-01 16:21:20 +0900

    練習問題2を作成

---
+++ 3-3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="result.php" method="post">
+        <input type="text" id="input" name="input">
+        <input type="submit" value="占う">
+        
+    </form>
+</body>
+</html>
